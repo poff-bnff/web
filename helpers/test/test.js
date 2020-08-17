8cc959ea65e5faf7e516be8c32fa2dbe6650fdc9
Martin Pennaste <pennaste04@gmail.com>
2020-08-17 17:45:26 +0300

    Copy template index file + option added

--- helpers/test/test.js
+++ helpers/test/test.js
@@ -12,41 +12,24 @@ var options = {
 };
 
 
-function getData(callback, lang) {
+function getData(callback, lang, copyFile) {
     request(options, function (error, response) {
         if (error)
             throw new Error(error);
         let data = new Object();
         data = JSON.parse(response.body);
-        callback(data, lang);
+        callback(data, lang, copyFile);
     });
 }
 
-function getDataCB(data, lang) {
+function getDataCB(data, lang, copyFile) {
 
     data.forEach(element => {
-        // console.log(element.slug_en);
         fs.mkdir(`helpers/test/${element.slug_en}`, bla);
 
         let elementEt = JSON.parse(JSON.stringify(element));
 
-        // Object.keys(elementEt).forEach(item => {
-        //     console.log(item.substring(item.length - 3, item.length));
-        //     let lastThree = item.substring(item.length - 3, item.length);
-        //     if (lastThree === '_en' || lastThree === '_ru') {
-        //         // delete elementEt[item];
-        //     }
-        //   });
-
-
         for (key in elementEt) {
-        // for (i = 0; i < Object.keys(elementEt).length; i++) {
-            // let item = elementEt[0];
-            // let item2 = item[Object.keys(item)];
-
-            // console.log('aaaaaaaaaaaa' + key);
-            // console.log('aaaaaaaaaaaa' + Object.keys(item));
-            // console.log(item.substring(item.length - 3, item.length));
             let lastThree = key.substring(key.length - 3, key.length);
             let findHyphen = key.substring(key.length - 3, key.length -2);
             if (lastThree !== `_${lang}` && findHyphen === '_' && lastThree !== '_by' && lastThree !== '_at') {
@@ -61,27 +44,25 @@ function getDataCB(data, lang) {
             }
         }
 
-
-        //   console.log(elementEt);
         let yamlStr = yaml.safeDump(elementEt, {'indent':'4'});
-        console.log(elementEt);
-        console.log('LANGUAAAAAG: ' + lang);
 
-        fs.writeFileSync(`helpers/test/${element.slug_en}/data.${lang}.yaml`, yamlStr, 'utf8', {space: 2});
-
-    // YAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAML END
-
+        fs.writeFileSync(`helpers/test/${element.slug_en}/data.${lang}.yaml`, yamlStr, 'utf8');
+        if (copyFile) {
+            fs.copyFile(`helpers/test/film_index_template.pug`, `helpers/test/${element.slug_en}/index.pug`, (err) => {
+                if (err) throw err;
+                console.log(`File was copied to folder ${element.slug_en}`);
+                })
+        }
     });
 
 }
 
 function bla() {
-    console.log('tere');
 }
 
-getData(getDataCB, "en");
-getData(getDataCB, "et");
-getData(getDataCB, "ru");
+getData(getDataCB, "en", 1);
+getData(getDataCB, "et", 0);
+getData(getDataCB, "ru", 0);
 
 
 
