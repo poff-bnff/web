8e4cf95b86844ff6f4d26edc5622bad0be2dd433
Martin Pennaste <pennaste04@gmail.com>
2020-08-17 12:17:12 +0300

    test.js

---
+++ helpers/test/test.js
@@ -0,0 +1,23 @@
+
+var request = require('request');
+var options = {
+  'method': 'GET',
+  'url': 'http://139.59.130.149/films',
+  'headers': {
+    'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNTk3MjQwNDA2LCJleHAiOjE1OTk4MzI0MDZ9.ZddzcDSe7O130sr4dtxr2XOSP7j-BTmlOI8TGxWgKdM'
+  }
+};
+
+
+function getData() {
+    request(options, function (error, response) {
+    if (error) throw new Error(error);
+    //   console.log(response.body);
+        let data = JSON.parse(response.body);
+    //   console.log(data);
+    });
+}
+console.log(bla);
+// for (x in data) {
+//     console.log('bla: ' + x.id);
+// }
