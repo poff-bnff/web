c63e8cb9e7651d551a950b96af1b9dbcae88ac85
Martin Pennaste <pennaste04@gmail.com>
2020-08-18 11:27:20 +0300

    Formatting code, excess removed

--- helpers/test/test.js
+++ helpers/test/test.js
@@ -5,12 +5,6 @@ const yaml = require('js-yaml');
 
 const allLanguages = ["en", "et", "ru"];
 
-console.log(process.env.StrapiPassword);
-console.log(process.env.StrapiUserName);
-
-
-// console.log(optionsAuth);
-
 function getToken() {
     let token = '';
 
@@ -18,9 +12,9 @@ function getToken() {
         'method': 'POST',
         'url': 'http://139.59.130.149/auth/local',
         'headers': {
-        'Content-Type': 'application/json'
+            'Content-Type': 'application/json'
         },
-        body: JSON.stringify({"identifier":process.env.StrapiUserName,"password":process.env.StrapiPassword})
+        body: JSON.stringify({ "identifier": process.env.StrapiUserName, "password": process.env.StrapiPassword })
     };
 
     request(optionsAuth, function (error, response) {
@@ -31,17 +25,16 @@ function getToken() {
             'method': 'GET',
             'url': 'http://139.59.130.149/films',
             'headers': {
-            'Authorization': 'Bearer ' + token
+                'Authorization': 'Bearer ' + token
             }
         };
 
-        console.log(token + 'bla');
+        // getData(function, new directory path, language, copy file)
+        getData("helpers/test/", "en", 1, getDataCB);
+        getData("helpers/test/", "et", 0, getDataCB);
+        getData("helpers/test/", "ru", 0, getDataCB);
 
-        getData(getDataCB, "helpers/test/", "en", 1);
-        getData(getDataCB, "helpers/test/", "et", 0);
-        getData(getDataCB, "helpers/test/", "ru", 0);
-
-        function getData(callback, dirPath, lang, copyFile) {
+        function getData(dirPath, lang, copyFile, callback) {
             request(options, function (error, response) {
                 if (error)
                     throw new Error(error);
@@ -52,7 +45,6 @@ function getToken() {
         }
 
         function getDataCB(data, dirPath, lang, copyFile) {
-            console.log(data);
             data.forEach(element => {
                 fs.mkdir(`${dirPath}${element.slug_en}`, bla);
 
@@ -60,20 +52,20 @@ function getToken() {
 
                 for (key in elementEt) {
                     let lastThree = key.substring(key.length - 3, key.length);
-                    let findHyphen = key.substring(key.length - 3, key.length -2);
+                    let findHyphen = key.substring(key.length - 3, key.length - 2);
                     if (lastThree !== `_${lang}` && findHyphen === '_' && !allLanguages.includes(lastThree)) {
                         delete elementEt[key];
                     }
-                    if(lastThree === `_${lang}`) {
-                        if(key.substring(0, key.length -3) == 'slug') {
+                    if (lastThree === `_${lang}`) {
+                        if (key.substring(0, key.length - 3) == 'slug') {
                             elementEt.path = elementEt[key];
                         }
-                        elementEt[key.substring(0, key.length -3)] = elementEt[key];
+                        elementEt[key.substring(0, key.length - 3)] = elementEt[key];
                         delete elementEt[key];
                     }
                 }
 
-                let yamlStr = yaml.safeDump(elementEt, {'indent':'4'});
+                let yamlStr = yaml.safeDump(elementEt, { 'indent': '4' });
 
                 fs.writeFileSync(`${dirPath}${element.slug_en}/data.${lang}.yaml`, yamlStr, 'utf8');
                 if (copyFile) {
@@ -95,7 +87,3 @@ function getToken() {
 
 getToken();
 
-// getData(function, new directory path, language, copy file)
-// for (x in data) {
-//     console.log('bla: ' + x.id);
-// }
