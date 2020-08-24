0533741f79b27a37e4dc384777d3d27e6800832f
LiisKasper <68702810+LiisKasper@users.noreply.github.com>
2020-08-24 15:07:57 +0300

    slugify fix komad

--- helpers/GoogleSheets/FilmsJSONfromSheets.js
+++ helpers/GoogleSheets/FilmsJSONfromSheets.js
@@ -120,9 +120,9 @@ function ProcessDataCB(source){
                                       'languages': ProcessLanguages(element['filmLanguages_en']),
                                     },
             'screenings': '',
-            'slug_et': slugify((element['filmTitle_et']), {lower:true, remove: /[*+~.()'"!:@]/g}),
-            'slug_en': slugify((element['filmTitle_en']), {lower:true, remove: /[*+~.()'"!:@]/g}),
-            'slug_ru': slugify('', {lower:true, remove: /[*+~.()'"!:@]/g}),
+            'slug_et': slugify((element['filmTitle_et']), {lower:true, remove: /[*+~.,()'"!:@]/g}),
+            'slug_en': slugify((element['filmTitle_en']), {lower:true, remove: /[*+~.,()'"!:@]/g}),
+            'slug_ru': slugify('', {lower:true, remove: /[*+~.,()'"!:@]/g}),
 
         };
         console.log(slugify(element['filmTitle_et']));
