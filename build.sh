28ec7aea9cf865ae93abe07f61e72567d05483a6
Martin Pennaste <pennaste04@gmail.com>
2020-08-27 02:16:40 +0300

    Single article defaultimage downloaded n displayed

--- build.sh
+++ build.sh
@@ -3,5 +3,6 @@
 # node ./helpers/compile_film_pictures.js
 # node ./helpers/compile_article_pictures.js
 
-cp -R assets build/assets
+mkdir build/assets
+cp -R assets/* build/assets/
 node ./node_modules/entu-ssg/src/build.js ./entu-ssg.yaml full
