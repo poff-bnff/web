aa1cf8395279256ad4b201b79154f747e5335be4
Mariann <tapferm@gmail.com>
2020-08-27 12:55:07 +0300

    UpDateAllFromStrapi värskendab strapist tõmmatud JSON failid

---
+++ helpers/strapi/UpdateAllFromStrapi.js
@@ -0,0 +1,12 @@
+const FromStrapi = require('./FromStrapi')
+
+
+function LogProcess(token, dataPath){
+    console.log("updating " + dataPath.slice(1) + " from Strapi");
+}
+
+
+FromStrapi.WriteJSON('/countries', '../data/ISOCountriesFromStrapi.json', LogProcess)
+FromStrapi.WriteJSON('/languages', '../data/ISOLanguages.json', LogProcess)
+FromStrapi.WriteJSON('/films', '../data/FilmsFromStrapi.json', LogProcess)
+FromStrapi.WriteJSON('/articles', '../data/articlesFromStrapi.json', LogProcess)
