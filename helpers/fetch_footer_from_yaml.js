5fe4f2688189dfe2f6861d04fbeb6ddb0dde1c28
Martin Pennaste <pennaste04@gmail.com>
2020-09-12 02:31:15 +0300

    test footer fetch

--- helpers/fetch_footer_from_yaml.js
+++ helpers/fetch_footer_from_yaml.js
@@ -88,7 +88,7 @@ function rueten(obj, lang) {
 
 function processData(data, lang, CreateYAML) {
     let copyData = JSON.parse(JSON.stringify(data));
-    // console.log(copyData);
+    console.log('COPYDATA: ' + copyData);
     let buffer = [];
     for (values in copyData) {
         // console.log(values)
