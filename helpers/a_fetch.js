19faa9d94bd8d4cb07712addb339da1d9c0d183d
LiisKasper <68702810+LiisKasper@users.noreply.github.com>
2020-09-17 17:42:47 +0300

    _fetchdir articles fix

--- helpers/a_fetch.js
+++ helpers/a_fetch.js
@@ -3,6 +3,11 @@ const fs = require('fs')
 const util = require('util')
 const { resolve } = require('path')
 const yaml = require('js-yaml')
+const path = require('path');
+
+const dirPath =  path.join(__dirname, '../source/_fetchdir/');
+
+fs.mkdirSync(dirPath, { recursive: true })
 
 const DATAMODEL = yaml.safeLoad(fs.readFileSync(__dirname + '/../docs/datamodel.yaml', 'utf8'))
 for (const key in DATAMODEL) {
