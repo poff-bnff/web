fb217051ed8a2afce66ab10547136deb4067a172
Martin Pennaste <pennaste04@gmail.com>
2020-09-17 18:05:31 +0300

    Viiekas fix

--- build.sh
+++ build.sh
@@ -41,9 +41,9 @@ printf '\n----------        FINISHED creating separate YAML files      ---------
 
 printf '\n----------                  Processing styles                ----------\n\n'
 node ./helpers/copy_styles_acc_to_domain.js
-printf '\n----------             Finished processing styles            ----------\n\n'
+printf '\n----------             Finished processing styles            ----------\n'
 
-printf '\n\n----------         Downloading all img from Strapi         ----------\n\n'
+printf '\n----------         Downloading all img from Strapi         ----------\n\n'
 node ./helpers/download_article_img.js
 node ./helpers/download_footer_img.js
 node ./helpers/download_teams_img.js
