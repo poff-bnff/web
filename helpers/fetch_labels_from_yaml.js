2b273f5ecd9294f2261a0cb1480b7b5b743927dd
Martin Pennaste <pennaste04@gmail.com>
2020-09-18 19:13:44 +0300

    Labels fetching

--- helpers/fetch_labels_from_yaml.js
+++ helpers/fetch_labels_from_yaml.js
@@ -2,43 +2,40 @@ const fs = require('fs')
 const yaml = require('js-yaml')
 const path = require('path')
 const rueten = require('./rueten.js')
-const util = require('util')
 
-const sourceDir =  path.join(__dirname, '..', 'source')
-const fetchDir =  path.join(sourceDir, '_fetchdir')
-const strapiDataPath = path.join(fetchDir, 'strapiData.yaml')
-const STRAPIDATA = yaml.safeLoad(fs.readFileSync(strapiDataPath, 'utf8'))['LabelGroup']
+const sourceDir =  path.join(__dirname, '..', 'source');
+const fetchDir =  path.join(sourceDir, '_fetchdir');
+const strapiDataPath = path.join(fetchDir, 'strapiData.yaml');
+const STRAPIDATA = yaml.safeLoad(fs.readFileSync(strapiDataPath, 'utf8'))['LabelGroup'];
 
-const languages = ['en', 'et', 'ru']
+const languages = ['en', 'et', 'ru'];
 for (const ix in languages) {
-    const lang = languages[ix]
-    let data = rueten(STRAPIDATA, lang)
-    // console.log(data)
-    processData(data, lang)
-    console.log(`Fetched ${process.env['DOMAIN']} labels ${lang} data`)
+    const lang = languages[ix];
+    let data = rueten(STRAPIDATA, lang);
+    processData(data, lang);
+    console.log(`Fetched ${process.env['DOMAIN']} labels ${lang} data`);
 }
 
 function processData(data, lang) {
-    var buffer = {}
-    var labels = {}
+    var buffer = {};
+    var labels = {};
     for (key in data) {
-        let smallBuffer = {}
-        // console.log(data[key].name)
-        var data2 = data[key]
-        var name = data[key].name
+        let smallBuffer = {};
+        var data2 = data[key];
+        var name = data[key].name;
         for (key2 in data2.label) {
-            smallBuffer[data2.label[key2].name] = data2.label[key2].value
+            smallBuffer[data2.label[key2].name] = data2.label[key2].value;
         }
-        buffer[name] = smallBuffer
-        labels[name] = buffer[name]
+        buffer[name] = smallBuffer;
+        labels[name] = buffer[name];
     }
-    // console.log(labels)
 
-    const globalDataSourceFile = path.join(sourceDir, `global.static.${lang}.yaml`)
-    const globalDataTargetFile = path.join(sourceDir, `global.${lang}.yaml`)
-    let globalData = yaml.safeLoad(fs.readFileSync(globalDataSourceFile, 'utf8'))
-    globalData.label = labels
-    // console.log(util.inspect(globalData, true, 18))
-    let allDataYAML = yaml.safeDump(globalData, { 'noRefs': true, 'indent': '4' })
-    fs.writeFileSync(globalDataTargetFile, allDataYAML, 'utf8')
+    const globalDataSourceFile = path.join(sourceDir, `global.static.${lang}.yaml`);
+    const globalDataTargetFile = path.join(sourceDir, `global.${lang}.yaml`);
+    let globalData = yaml.safeLoad(fs.readFileSync(globalDataSourceFile, 'utf8'));
+    globalData.label = JSON.parse(JSON.stringify(labels));
+
+    // , skipInvalid: true - töötab ka kui ei tee koopiat
+    let allDataYAML = yaml.safeDump(globalData, { 'noRefs': true, 'indent': '4'});
+    fs.writeFileSync(globalDataTargetFile, allDataYAML, {encoding:'utf8', flag:'w'});
 }
