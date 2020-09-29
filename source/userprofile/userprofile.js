5898cb41cd9fcdff57be98aaa2f20d4f992c6bdc
Mariann <tapferm@gmail.com>
2020-09-29 23:08:13 +0300

    userprofile paneb kokku listi objektidest mida saata cognitosse
    võtab lehe väljadele sisestatud data ja paneb kokku listiks, mis võiks sobida kognitosse saatmiseks? Hetkel logib SALVESTA nupu vajutamisel consolile

--- source/userprofile/userprofile.js
+++ source/userprofile/userprofile.js
@@ -1,10 +1,15 @@
-let userProfile ={}
 
-let firstName = document.getElementById("firstName");
-let lastName = document.getElementById("lastName");
-let email = document.getElementById("email");
 
-async function loadInfoFromIdtkn(){
+let profileImg = document.getElementById("profileImg")
+let firstName = document.getElementById("firstName")
+let lastName = document.getElementById("lastName")
+let dob = document.getElementById("dob")
+let phoneNr = document.getElementById("phoneNr")
+let email = document.getElementById("email")
+let country = document.getElementById("country")
+let city = document.getElementById("city")
+
+async function LoadUserInfo(){
     let response = await fetch(`https://api.poff.ee/profile`, {
             method: 'GET',
             headers: {
@@ -12,13 +17,33 @@ async function loadInfoFromIdtkn(){
         });
         userProfile = await response.json()
 
-        //return response.json()
-        console.log(userProfile)
         firstName.value=userProfile.name
         lastName.value=userProfile.family_name
         email.value=userProfile.email
 }
-loadInfoFromIdtkn()
+LoadUserInfo()
+
+
+
+function sendUserProfile(){
+    //siin panen kokku listi objektidest mida saata cognitosse
+    //Kas järjekord on oluline?
+    //kas Name-id on kõik õiged?
+    let userToSend=[
+        {Name: "picture", Value: profileImg.value},
+        {Name: "name", Value: firstName.value},
+        {Name: "family_name", Value: lastName.value},
+        {Name: "gender", Value: $('input[name="gender"]:checked').val()},
+        {Name: "birthdate", Value: dob.value},
+        {Name: "phone", Value: phoneNr.value},
+        {Name: "email", Value: email.value},
+        {Name: "address", Value: `${country.value}, ${city.value}`},
+        ]
+
+    console.log(userToSend)
+}
+
+
 
 
 
