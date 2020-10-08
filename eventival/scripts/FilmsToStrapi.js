a05b969432acd96fd557e5dddb40d30e0d5ece90
Liis <liis.kasper@gmail.com>
2020-10-08 12:18:52 +0300

    in progress

--- eventival/scripts/FilmsToStrapi.js
+++ eventival/scripts/FilmsToStrapi.js
@@ -13,24 +13,6 @@ const yamlStr = fs.readFileSync(films_fn)
 const FILMS_API = 'http://139.59.130.149/films'
 const CASSETTES_API = 'http://139.59.130.149/cassettes'
 
-// function Countries(txt){
-//     let countryNames = []
-//     let countryCodes = []
-//     let countryFromISOcountries = JSON.parse(fs.readFileSync()
-//     countryNames.forEach(name => {
-//         let found = {'name': name};
-//         .forEach(value => {
-//             // console.log(value);
-//             if (name == value['name_en']){
-//                 found['id'] = value['id']
-//             }
-//         } )
-//         countryCodes.push(found)
-//     })
-//     console.log(countryCodes)
-//     return countryCodes
-// }
-
 const eventivalFilms = (yaml.safeLoad(yamlStr).map(film => {
     let filmOut = {
         filmId: film.ids.system_id.toString(),
@@ -39,7 +21,7 @@ const eventivalFilms = (yaml.safeLoad(yamlStr).map(film => {
         title_ru: film.titles.title_custom,
         titleOriginal: film.titles.title_original,
         year: film_info.completion_date.year,
-        runtime: film.film_info.runtime.seconds / 60,
+        runtime: (film.film_info.runtime.seconds / 60).toString(),
         festival_edition: film.eventival_categorization.categories.category,
         otherFestivals: '?',
         synopsis: {
@@ -47,53 +29,54 @@ const eventivalFilms = (yaml.safeLoad(yamlStr).map(film => {
             en: film.publications.en.synopsis_long,
             ru: film.publications.ru.synopsis_long
         },
-        // tags: {
-        //     premiere_types: '?',
-        //     genres: film.film_info.types.type,
-        //     keywords: film.eventival_categorization.tags.tag,
-        //     programmes: {
-        //         id: film.eventival_categorization.sections.section.id
-        //     },
-        //     countriesAndLanguages: {
-        //         countries: {
-        //             id: film.film_info.countries.country.code
-        //         },
-        //         languages: {
-        //             id: film.film_info.languages.language.code
-        //         }
-        //     },
-        //     subtitles: {
-        //         id: film.film_info.subtitle_languages.subtitle_language.code
-        //     }
-        // },
-        // credentials: {
-        //     rolePerson: {
-        //         role_at_film: {
-        //             id: 0,
-        //             roleName: {
-        //                 et: '?',
-        //                 en: '?',
-        //                 ru: '?'
-        //             }
-        //         },
-        //         person: {
-        //             id: '?'
-        //         }
-        //     },
-        //     roleCompany: {
-        //         role_at_film: { 
-        //             roleName : { 
-        //                 et: '?',
-        //                 en: '?',
-        //                 ru: '?'
-        //             }
-        //         },
-        //         person: {
-        //             id : '?' 
-        //         }
-        //     }
-            
-        // },
+        tags: {
+            premiere_types: '?',
+            genres: film.film_info.types.type,
+            keywords: film.eventival_categorization.tags.tag,
+            programmes: {
+                id: film.eventival_categorization.sections.section.id
+            }
+        },
+        countriesAndLanguages: {
+            countries: {
+                id: film.film_info.countries.country.code
+            },
+            languages: {
+                id: film.film_info.languages.language.code
+            }
+        },
+        subtitles: {
+            id: film.film_info.subtitle_languages.subtitle_language.code
+        },
+        credentials: {
+            rolePerson: {
+                role_at_film: {
+                    id: film.publications.en....
+                },
+                person: {
+                    id: '?'
+                }
+            },
+            roleCompany: {
+                role_at_film: {
+                    id: '?'
+                },
+                person: {
+                    id : '?'
+                }
+            }
+        },
+        presentedBy: {
+            presentedBText: {
+                et: '?',
+                en: '?',
+                ru: '?'
+            },
+            organisations: {
+                id: '?'
+            }
+        },
+        world_sales: '?'
 
     }
 
@@ -102,10 +85,33 @@ const eventivalFilms = (yaml.safeLoad(yamlStr).map(film => {
 
 const eventivalCassettes = (yaml.safeLoad(yamlStr).map(film => {
     let cassetteOut = {
-        remoteId: film.ids.system_id,
+        remoteId: film.ids.system_id.toString(),
         title_et: film.titles.title_local,
         title_en: film.titles.title_english,
-        title_ru: film.titles.title_custom
+        title_ru: film.titles.title_custom,
+        synopsis: {
+            et: film.publications.et.synopsis_long,
+            en: film.publications.en.synopsis_long,
+            ru: film.publications.ru.synopsis_long
+        },
+        festival_edition: film.eventival_categorization.categories.category,
+        tags: {
+            premiere_types: '?',
+            genres: film.film_info.types.type,
+            keywords: film.eventival_categorization.tags.tag,
+            programmes: {
+                id: film.eventival_categorization.sections.section.id
+            }
+        },
+        slug_et: '?',
+        slug_en: '?',
+        slug_ru: '?',
+        presenters: {
+            id: '?'
+        },
+        films: {
+            id: '?'
+        }
     }
 
     return cassetteOut
@@ -115,7 +121,7 @@ const eventivalCassettes = (yaml.safeLoad(yamlStr).map(film => {
 
 const submitCassettes = async () => {
     const token = await strapiAuth()
-    
+
     for (const ix in eventivalCassettes) {
         if (ix > 5) {
             continue
@@ -138,12 +144,12 @@ const submitCassettes = async () => {
             e_cassette['id'] = strapiCassette[0].id
             options.path = CASSETTES_API + '/' + e_cassette.id
             options.method = 'PUT'
-            
+
             fromStrapi = await strapiQuery(options, e_cassette)
         } else {
             options.path = CASSETTES_API
             options.method = 'POST'
-            
+
             fromStrapi = await strapiQuery(options, e_cassette)
         }
         // console.log(fromStrapi)
@@ -152,7 +158,7 @@ const submitCassettes = async () => {
 
 const submitFilms = async () => {
     const token = await strapiAuth()
-    
+
     for (const ix in eventivalFilms) {
         if (ix > 5) {
             continue
@@ -175,12 +181,12 @@ const submitFilms = async () => {
             e_film['id'] = strapiFilm[0].id
             options.path = FILMS_API + '/' + e_film.id
             options.method = 'PUT'
-            
+
             fromStrapi = await strapiQuery(options, e_film)
         } else {
             options.path = FILMS_API
             options.method = 'POST'
-            
+
             fromStrapi = await strapiQuery(options, e_film)
         }
         // console.log(fromStrapi)
