9698e4c2effd4f8a1a151af188c478e33bc58448
SiimS1 <siimsutt@hotmail.com>
2020-10-13 15:03:42 +0300

    x

--- source/favourite/script.js
+++ source/favourite/script.js
@@ -2,7 +2,6 @@
 document.addEventListener("DOMContentLoaded", loadMyFavouriteFilms, false);
 
 async function loadMyFavouriteFilms(e){
-    console.log('hello');
 
     var response = await fetch(`https://api.poff.ee/favourite`, {
         method: 'GET',
@@ -13,12 +12,9 @@ async function loadMyFavouriteFilms(e){
 
     var favouriteFilms2 = await response.json()
 
-    console.log(response, 'loadmyFavs info ', favouriteFilms2.films)
-
     var film_cards = document.getElementsByClassName('card_film')
     for (var film_card of film_cards) {
         var filmId = parseInt(film_card.id)
-        // console.log(filmId);
         if (filmId in favouriteFilms2.films) {
             film_card.style.display = 'block'
         }
@@ -26,8 +22,19 @@ async function loadMyFavouriteFilms(e){
 }
 
 
-function removeFilm(){
-    console.log('hello');
+async function removeFilm(movieId){
+    console.log('removeFilm')
+    console.log(movieId)
+
+    var response = await fetch(`https://api.poff.ee/favourite/${movieId}`, {
+        method: 'DELETE',
+        headers: {
+            Authorization: 'Bearer ' + localStorage.getItem('ACCESS_TOKEN'),
+        }
+    });
+
+    let result = await response.json()
+    console.log(result.todo);
 }
 
 
