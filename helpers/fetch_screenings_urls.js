8ed7d32562463b171837a7b15ccd1c9bc5da170d
michelek <mihkel.putrinsh@gmail.com>
2020-10-14 02:13:51 +0300

    ticketing url to strapi screenings

--- helpers/fetch_screenings_urls.js
+++ helpers/fetch_screenings_urls.js
@@ -4,9 +4,7 @@ const path = require('path')
 const request = require('request');
 const sourceDir = path.join(__dirname, '..', 'source')
 const fetchDir = path.join(sourceDir, '_fetchdir')
-const { getModel } = require("./strapiQuery.js")
-const STRAPI_URL = process.env['StrapiHost']
-const SCREENINGS_API = `${STRAPI_URL}/screenings`
+const { getModel, putModel } = require("./strapiQuery.js")
 
 async function main() {
 
@@ -23,7 +21,7 @@ async function main() {
         'url': 'http://www.piletilevi.ee/api/?preset=pff&language=est',
         'headers': {}
     };
-
+    let PL_screenings = []
     request(optionsPL, async function (error, response) {
         if (error) throw new Error(error);
 
@@ -31,21 +29,37 @@ async function main() {
             yaml.safeDump([], { 'noRefs': true, 'indent': '4' })
         } else {
             const concerts = JSON.parse(response.body).responseData.concert
-            const screenings = concerts.map(concert => {
+            PL_screenings = concerts.map(concert => {
                 return {
                     codeAndTitle: concert.decoratedTitle || null,
                     code: concert.decoratedTitle ? concert.decoratedTitle.split(' / ')[0] : null,
                     ticketingUrl: concert.shopUrl || null,
-                    remoteId: concert.id || null,
+                    ticketingId: concert.id.toString() || null,
                     // salesTime: concert.salesTime || null
                 }
-            })
-            var YAML = yaml.safeDump(screenings, { 'noRefs': true, 'indent': '4' })
-            const outFile = path.join(fetchDir, `screenings_urls.yaml`)
-            fs.writeFileSync(outFile, YAML, 'utf8')
+            }).filter(PL_screening => PL_screening.code !== null)
+            // var YAML = yaml.safeDump(PL_screenings, { 'noRefs': true, 'indent': '4' })
+            // const outFile = path.join(fetchDir, `screenings_urls.yaml`)
+            // fs.writeFileSync(outFile, YAML, 'utf8')
         }
     })
-    console.log(JSON.stringify(await getModel('Cinema'), null, 4))
+    // console.log(null === null)
+    const s_screenings = await getModel('Screening')
+
+    for (const PL_screening of PL_screenings) {
+        let s_screening = s_screenings.filter(s_screening => {
+            // console.log(s_screening.remoteId, PL_screening.remoteId.toString())
+            return s_screening.code === PL_screening.code
+        })[0] || {code: null}
+        PL_screening.id = s_screening.id
+    }
+
+    const outFile = path.join(fetchDir, `screenings_urls.yaml`)
+    fs.writeFileSync(outFile, JSON.stringify(PL_screenings, null, 4), 'utf8')
+
+
+
+    await putModel('Screening', PL_screenings)
 }
 
 main()
