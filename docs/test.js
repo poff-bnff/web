c27041e434b936e614edfd1df4c067c405e5156e
Liis <liis.kasper@gmail.com>
2020-10-19 14:37:25 +0300

    ajutine

---
+++ docs/test.js
@@ -0,0 +1,15 @@
+
+        // let newD = new Date(e_screening.start + ET.utc2)
+        // strapi_screening.dateTime = newD
+
+        let nDate = new Date()
+        let d = new Date ('2020-11-13 19:00:00')
+        let utc = Date.UTC(2020, 11, 13, 19, 00, 00)
+        console.log(utc);
+
+        // // let momentD = moment(d)
+        // let zoneD = atZone(d)
+        // console.log(nDate, '--------', d, '---', zoneD)
+
+        // let n = d.toLocaleString('fi-FI')
+        // console.log(d, '----', n);
