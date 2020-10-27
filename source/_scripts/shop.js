d0ceefd7e374e015eba0dbdae1c4b4a356b4e43b
Mariann <tapferm@gmail.com>
2020-10-27 13:38:46 +0200

    shopping in progress

--- source/_scripts/shop.js
+++ source/_scripts/shop.js
@@ -29,7 +29,20 @@ function BuyProduct(categoryId) {
 
 }
 
+var paymentType = "valimata"
+
+function SelectPaymentType(id){
+    paymentType= id
+    console.log("sinu valitud makseviis on " + paymentType)
+}
+
+function Buy(productCode){
+    console.log("ostad toote "+productCode+" ja maksad " + paymentType)
+}
+
 function GetPaymentLinks() {
+
+    var links = document.getElementById("paymentLinks")
 
     var myHeaders = new Headers();
     myHeaders.append('Authorization', 'Bearer ' + localStorage.getItem('ACCESS_TOKEN'));
@@ -49,7 +62,23 @@ function GetPaymentLinks() {
         return Promise.reject(response);
     }).then(function (data) {
         console.log(data);
-        return data
+        var bankInfo= ""
+        //pangalingid
+        for (var i = 0; i < data.banklinks.length; i++){
+            if (data.banklinks[i].country === "ee"){
+                var button = '<img src='+ data.banklinks[i].logo +' onClick=SelectPaymentType("'+data.banklinks[i].id+'") >'
+                console.log(data.banklinks[i].logo)
+                bankInfo += button
+            }
+        }
+        //credist cards
+        for (var i = 0; i < data.cards.length; i++){
+            var button = '<img src='+ data.cards[i].logo +' onClick=SelectPaymentType("'+data.cards[i].id+'") >'
+            console.log(data.cards[i].logo)
+            bankInfo += button
+
+        }
+        links.innerHTML = bankInfo
 
     }).catch(function (error) {
         console.warn(error);
