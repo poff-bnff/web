58e523e77388987b29141311d1ed1434ad37e167
Martin Pennaste <pennaste04@gmail.com>
2020-11-02 23:49:47 +0200

    Fix empty film build fail and display cassettes without films.

--- helpers/fetch_cassettes_from_yaml.js
+++ helpers/fetch_cassettes_from_yaml.js
@@ -182,13 +182,17 @@ for (const lang of allLanguages) {
             }
 
             // #379 put ordered films to cassette.film
-            let ordered_films = s_cassette_copy.orderedFilms.map(s_c_film => {
+            let ordered_films = s_cassette_copy.orderedFilms
+                .filter( (isFilm) => { if (isFilm.film) { return 1 } else { console.log(`ERROR! Empty film under cassette with ID ${s_cassette_copy.id}`) } })
+                .map(s_c_film => {
+
                 if (!s_c_film.film) {
                     // console.log('ERROR: Cassette with no ordered film', s_cassette_copy.id);
                     cassettesWithOutFilms.push(s_cassette_copy.id)
                     // throw new Error('Cassette with no ordered film')
                 } else {
                     let s_films = STRAPIDATA_FILMS.filter( (s_film) => { return s_c_film.film.id === s_film.id } )
+
                     if (s_films && s_films[0]) {
                         s_films[0].ordinal = s_c_film.order
                         return s_films[0]
@@ -284,6 +288,7 @@ for (const lang of allLanguages) {
 
             if (s_cassette_copy.films && s_cassette_copy.films[0]) {
                 for (scc_film of s_cassette_copy.films) {
+                    // console.log(scc_film);
                     let filmSlugEn = scc_film.slug_en
 
                     if (!filmSlugEn) {
