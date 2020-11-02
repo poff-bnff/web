b8e779423daf0fdc484512158be85266c8c37e54
Liis <liis.kasper@gmail.com>
2020-11-02 12:18:59 +0200

    in progress

--- source/mypoff/script.js
+++ source/mypoff/script.js
@@ -1,4 +1,14 @@
 
+
+// var qrcode = new QRCode("qrcode")
+
+// function makeCode () {
+// 	qrcode.makeCode('elText.value')
+// }
+
+// makeCode()
+
+
 fetchMyPasses()
 
 async function fetchMyPasses(){
@@ -12,47 +22,25 @@ async function fetchMyPasses(){
       redirect: 'follow'
     };
 
-    let response = await fetch("https://api.poff.ee/product", requestOptions)
+    var response = await fetch("https://api.poff.ee/product", requestOptions)
 
-    console.log(await response.json());
+    var my_passes = await response.json();
+
+    var my_passes_element = document.getElementById('my_passes')
+    console.log(my_passes_element);
+    var ix = 0
+    for (my_pass of my_passes) {
+        ix ++
+        console.log(my_pass);
+
+        var pass_template = document.getElementById('template_' + my_pass.categoryId)
+        var my_pass_element = pass_template.cloneNode(true)
+
+        my_pass_element.setAttribute('ix', ix)
+        my_pass_element.style.display = 'block'
+
+        my_passes_element.appendChild(my_pass_element)
+
+
+    }
 }
-
-
-
-
-// function loadUserPass() {
-//     console.log('laen cognitost kasutaja passi andmed....')
-//     var myHeaders = new Headers()
-//     myHeaders.append('Authorization', 'Bearer ' + localStorage.getItem('ACCESS_TOKEN'))
-
-//     var requestOptions = {
-//         method: 'GET',
-//         headers: myHeaders,
-//         redirect: 'follow'
-//     }
-
-//     fetch('https://api.poff.ee/product', requestOptions).then(function (response) {
-//         if (response.ok) {
-//             return response.json()
-//         }
-//         return Promise.reject(response)
-//     }).then(function (data) {
-//         let passInfo = data
-//         useUserData(data)
-//         console.log("cognitos olev passi info:")
-//         console.log(passInfo)
-
-//     }).catch(function (error) {
-//         console.warn(error)
-//     })
-// }
-// loeadUserInfo
-// console.log(passInfo);
-
-var qrcode = new QRCode("qrcode")
-
-function makeCode () {
-	qrcode.makeCode('elText.value')
-}
-
-makeCode()
