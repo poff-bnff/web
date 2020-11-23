309444c846f0891aec32f2c358fd6eb11cf09049
SiimS1 <siimsutt@hotmail.com>
2020-11-23 19:30:54 +0200

    mycalendarbuttons ready

--- source/industry_mycal/script.js
+++ source/industry_mycal/script.js
@@ -3,19 +3,6 @@
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 
-// function calendarfile(id) {
-//     var element = document.createElement('a');
-//     element.setAttribute('href', 'data:text/calendar;charset=utf-8,' + document.getElementById(`cal_${id}`).innerText);
-//     // data:text/calendar;charset=utf-8
-//     element.setAttribute('download', `IndustryEvent_${id}.ics`);
-
-//     element.style.display = 'none';
-//     document.body.appendChild(element);
-
-//     element.click();
-
-//     document.body.removeChild(element);
-// }
 
 function showcalendarbuttons() {
     let calendarbuttons = document.querySelectorAll('[class~="add_to_calendar_button"]')
