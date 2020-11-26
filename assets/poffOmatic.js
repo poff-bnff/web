dcd37267a59a6161a7bf282bf49f7c46403e3e15
michelek <mihkel.putrinsh@gmail.com>
2020-11-27 01:34:38 +0200

    chat-o-matic ver.2

--- assets/poffOmatic.js
+++ assets/poffOmatic.js
@@ -15,13 +15,13 @@ function poff_chat_main () {
             return
         }
         if (SOCKET) {
-            console.log('Enjoy already existing socket', { ...HEADER })
+            // console.log('Enjoy already existing socket', { ...HEADER })
             return
         }
         HEADER.USER_ID = _user_id
         HEADER.LOCATION = slugify(_location)
         SOCKET = io(CHAT_SERVER)
-        console.log('Brand new socket for you', { ...HEADER })
+        // console.log('Brand new socket for you', { ...HEADER })
         SOCKET.on('ping', () => {
             SOCKET.emit('pong', { ...HEADER })
         })
@@ -29,15 +29,15 @@ function poff_chat_main () {
 
     function subscribe(message_type, callback) {
         if (SUBSCRIBERS.hasOwnProperty(message_type)) {
-            console.log({'P-O-M': 'First subscriber to', message_type, HEADER})
+            // console.log({'P-O-M': 'First subscriber to', message_type, HEADER})
             SUBSCRIBERS[message_type].push(callback)
         } else {
-            console.log({'P-O-M': 'Adding subscriber to', message_type, HEADER})
+            // console.log({'P-O-M': 'Adding subscriber to', message_type, HEADER})
             SUBSCRIBERS[message_type] = [callback]
             SOCKET.on(message_type, (message) => {
                 for (const cbf of SUBSCRIBERS[message_type]) {
                     if (!message) {
-                        console.log({'P-O-M': 'Empty message', Socket: SOCKET.id, message_type, message, HEADER})
+                        // console.log({'P-O-M': 'Empty message', Socket: SOCKET.id, message_type, message, HEADER})
                         continue
                     }
                     cbf(message)
